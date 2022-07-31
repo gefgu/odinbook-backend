efa98371aae606c16eb1c26dcec7f69feb40d42d
gefgu <gefgu@hotmail.com>
2022-07-31 16:42:47 -0300

    Add population using async of users, posts and comments

--- src/seeds.js
+++ src/seeds.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
-import { faker } from "@faker-js/faker";
+const faker = require("@faker-js/faker").faker;
+const async = require("async");
 
 console.log(
   "This script populates some test users, posts, comments to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true"
@@ -27,88 +28,106 @@ mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-function createRandomUser() {
-  return {
-    name: `${faker.name.firstName} ${faker.name.lastName}`,
+let users = [];
+let posts = [];
+let comments = [];
+
+function createRandomUser(callback) {
+  const user = new User({
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
     photoURL: faker.image.avatar(),
-  };
-}
-
-function createRandomPost(author) {
-  return {
-    content: faker.lorem.paragraphs(),
-    author: author,
-  };
-}
-
-function createRandomComment(author, post) {
-  return {
-    content: faker.lorem.paragraph(),
-    post: post,
-    author: author,
-  };
-}
-
-function populateDatabase() {
-  let users = [];
-  let posts = [];
-  let comments = [];
-
-  for (let i = 0; i < 5; i++) {
-    const user = new User(createRandomUser());
-    user.save(function (err) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(`New User: ${user}`);
-      users.push(user);
-    });
-  }
-
-  for (let i = 0; i < 10; i++) {
-    const post = new Post(createRandomPost(users[Math.floor(i / 2)]));
-    post.save(function (err) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(`New post: ${post}`);
-      posts.push(post);
-    });
-  }
-
-  for (let i = 0; i < 15; i++) {
-    const comment = new Comment(
-      createRandomComment(
-        users[Math.floor(i / 3)],
-        posts[users[Math.floor(i / 3)]]
-      )
-    );
-    comment.save(function (err) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(`New comment: ${comment}`);
-      comments.push(comment);
-    });
-  }
-
-  users.forEach((user, index) => {
-    let newUser = user;
-    let possibleFriends = users.splice(index, 1);
-    newUser.friends = possibleFriends.slice(0, index);
-    newUser.friendshipRequests = possibleFriends.slice(index);
-    User.findByIdAndUpdate(user._id, newUser, {}, function (err) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-
-      console.log(`Added friends and friendship requests for user ${newUser}`);
-    });
+  });
+  user.save(function (err) {
+    if (err) {
+      callback(err);
+      return;
+    }
+    console.log(`New User: ${user}`);
+    users.push(user);
+    callback(null, user);
   });
 }
 
-populateDatabase();
+function createRandomPost(i, callback) {
+  const post = new Post({
+    content: faker.lorem.paragraphs(),
+    author: users[Math.floor(i / 2)],
+  });
+  post.save(function (err) {
+    if (err) {
+      callback(err);
+      return;
+    }
+    console.log(`New post: ${post}`);
+    posts.push(post);
+    callback(null, post);
+  });
+}
+
+function createRandomComment(i, callback) {
+  const comment = new Comment({
+    content: faker.lorem.paragraph(),
+    author: users[Math.floor(i / 3)],
+    post: posts[Math.floor(i / 3)],
+  });
+  comment.save(function (err) {
+    if (err) {
+      callback(err);
+      return;
+    }
+    console.log(`New comment: ${comment}`);
+    comments.push(comment);
+    callback(null, comment);
+  });
+}
+
+// function createFriendsAndRequests(user, index, callback) {
+//   let newUser = user;
+//   let possibleFriends = users.splice(index, 1);
+//   newUser.friends = possibleFriends.slice(0, index);
+//   newUser.friendshipRequests = possibleFriends.slice(index);
+//   User.findByIdAndUpdate(user._id, newUser, {}, function (err) {
+//     if (err) {
+//       callback(err);
+//       return;
+//     }
+
+//     console.log(`Added friends and friendship requests for user ${newUser}`);
+//   });
+// }
+
+async.series(
+  [
+    (callback) =>
+      async.each(
+        [...Array(5).keys()],
+        (index, cb) => createRandomUser(cb),
+        callback
+      ),
+    (callback) =>
+      async.each(
+        [...Array(10).keys()],
+        (index, cb) => createRandomPost(index, cb),
+        callback
+      ),
+    (callback) =>
+      async.each(
+        [...Array(15).keys()],
+        (index, cb) => createRandomComment(index, cb),
+        callback
+      ),
+    // (callback) =>
+    //   async.forEachOf(
+    //     users,
+    //     (user, index, cb) => {
+    //       createFriendsAndRequests(user, index, cb);
+    //     },
+    //     callback
+    //   ),
+  ],
+  function (err, results) {
+    if (err) console.log(`Final Error: ${err}`);
+
+    mongoose.connection.close();
+  }
+);
