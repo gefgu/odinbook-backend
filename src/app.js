42b925b63a07999a06faa5c59cc976070b7b368c
gefgu <gefgu@hotmail.com>
2022-08-01 14:36:29 -0300

    Add mongoose connection

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const passport = require("passport");
 const authRouter = require("./routes/authRouter");
 const userRouter = require("./routes/userRouter");
@@ -7,6 +8,11 @@ const facebookStrategy = require("./strategies/facebookStrategy");
 const models = require("./models");
 
 const app = express();
+
+const mongoDb = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ercua.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose.connect(mongoDb, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 passport.use(facebookStrategy);
 
