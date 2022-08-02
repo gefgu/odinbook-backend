ebc549e6784f11b4881dd6e129e256e11604a804
gefgu <gefgu@hotmail.com>
2022-08-02 16:20:25 -0300

    Add GET from posts of user's friends

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -6,7 +6,13 @@ const postRouter = express.Router();
 postRouter.use(passport.authenticate("facebook-token", { session: false }));
 
 postRouter.get("/", (req, res, next) => {
-  res.json("Posts");
+  const friends = req.user.friends;
+
+  req.context.models.Post.find({ author: friends }).exec((err, postList) => {
+    if (err) return next(err);
+
+    res.json(postList);
+  });
 });
 
 module.exports = postRouter;
