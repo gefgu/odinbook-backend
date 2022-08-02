76d17d3e05eb3568849dc1aa645621baf4c5b164
gefgu <gefgu@hotmail.com>
2022-08-02 16:42:07 -0300

    Add PUT posts/:postId

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -9,11 +9,13 @@ postRouter.use(passport.authenticate("facebook-token", { session: false }));
 postRouter.get("/", (req, res, next) => {
   const friends = req.user.friends;
 
-  req.context.models.Post.find({ author: friends.concat(req.user._id) }).exec((err, postList) => {
-    if (err) return next(err);
+  req.context.models.Post.find({ author: friends.concat(req.user._id) }).exec(
+    (err, postList) => {
+      if (err) return next(err);
 
-    res.json(postList);
-  });
+      res.json(postList);
+    }
+  );
 });
 
 postRouter.post("/", [
@@ -49,5 +51,39 @@ postRouter.get("/:postId", (req, res, next) => {
     res.json(post);
   });
 });
+
+postRouter.put("/:postId", [
+  body("content", "Post Content must be specified").trim().isLength({ min: 1 }),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.array().length > 0) {
+      res.json(errors);
+      return;
+    }
+
+    const post = new req.context.models.Post({
+      content: req.body.content,
+      author: req.user._id,
+      _id: req.params.postId,
+    });
+
+    req.context.models.Post.findByIdAndUpdate(
+      req.params.postId,
+      post,
+      {},
+      function (err) {
+        if (err) return next(err);
+
+        if (post === null) {
+          const err = new Error("Post not found");
+          err.status = 404;
+          return next(err);
+        }
+
+        res.json({ message: "POST UPDATED WITH SUCESS!", post });
+      }
+    );
+  },
+]);
 
 module.exports = postRouter;
